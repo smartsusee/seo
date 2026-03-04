20e9d993f6d22424643bd0360449c87c261b6a0c
smartsusee <susee37432@gmail.com>
2026-03-04 12:15:35 +0530

    seo setup in laptop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,10 @@ function Home() {
           name="description"
           content="Kitkat Software Technologies provides web development and application services."
         />
+        <meta
+          name="keywords"
+          content="kitkat software technologies, web development company coimbatore, software company tamil nadu"
+        />
       </Helmet>
 
       <h1>Welcome to Kitkat Software Technologies</h1>
