6aba213351f007818a211a75d307d252cc95ac51
smartsusee <susee37432@gmail.com>
2026-03-04 12:47:23 +0530

    change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
         />
       </Helmet>
 
-      <h1>Welcome to Kitkat Software Technologies 1</h1>
+      <h1>Welcome to Kitkat Software Technologies </h1>
     </>
   );
 }
