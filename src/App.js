22e51b0795d80b8a83e397c554a3e5952ee88c07
smartsusee <susee37432@gmail.com>
2026-03-04 13:05:15 +0530

    change something

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Career from "./pages/Career";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
     <>
-     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/career">Career</Link>
-      </nav>
+      <HelmetProvider>
+        {" "}
+        {/* Wrapper add pannu */}
+        <BrowserRouter>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+            <Link to="/career">Career</Link>
+          </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/career" element={<Career />} />
-      </Routes>
-    </BrowserRouter>
-
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/career" element={<Career />} />
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
     </>
   );
 }
