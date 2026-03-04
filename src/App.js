a2229a814bc5d662516f0e650744dcb8746d4957
smartsusee <susee37432@gmail.com>
2026-03-04 12:35:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Career from "./pages/Career";
@@ -6,13 +6,20 @@ import Career from "./pages/Career";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/career" element={<Career />} />
-        </Routes>
-      </BrowserRouter>
+     <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/career">Career</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/career" element={<Career />} />
+      </Routes>
+    </BrowserRouter>
+
     </>
   );
 }
