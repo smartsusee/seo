f06d1512d4cbb80262dfca44750723cc35c7caab
smartsusee <susee37432@gmail.com>
2026-03-04 12:23:03 +0530

    change code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
